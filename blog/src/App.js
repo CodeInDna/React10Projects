f6989b53cf72bdc3b1caaa8a83006e856dafe2e8
ykarelia323@gmail.com <ykarelia323@gmail.com>
2019-07-25 11:18:24 +0530

    Integrate the template inside React Library

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,50 +1,15 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Navbar from './Navbar';
+import Header from './Header';
+import Footer from './Footer';
 
 class App extends Component {
   render(){
       return (
         <div>
-          <nav className="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
-            <div className="container">
-              <a className="navbar-brand" href="index.html">Start Bootstrap</a>
-              <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-                Menu
-                <i className="fas fa-bars"></i>
-              </button>
-              <div className="collapse navbar-collapse" id="navbarResponsive">
-                <ul className="navbar-nav ml-auto">
-                  <li className="nav-item">
-                    <a className="nav-link" href="index.html">Home</a>
-                  </li>
-                  <li className="nav-item">
-                    <a className="nav-link" href="about.html">About</a>
-                  </li>
-                  <li className="nav-item">
-                    <a className="nav-link" href="post.html">Sample Post</a>
-                  </li>
-                  <li className="nav-item">
-                    <a className="nav-link" href="contact.html">Contact</a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </nav>
+          <Navbar />
 
-          <header className="masthead" >
-            <div className="overlay"></div>
-            <div className="container">
-              <div className="row">
-                <div className="col-lg-8 col-md-10 mx-auto">
-                  <div className="site-heading">
-                    <h1>Clean Blog</h1>
-                    <span className="subheading">A Blog Theme by Start Bootstrap</span>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </header>
+          <Header />
 
           <div className="container">
             <div className="row">
@@ -111,41 +76,7 @@ class App extends Component {
 
           <hr/>
 
-          <footer>
-            <div className="container">
-              <div className="row">
-                <div className="col-lg-8 col-md-10 mx-auto">
-                  <ul className="list-inline text-center">
-                    <li className="list-inline-item">
-                      <a href="#">
-                        <span className="fa-stack fa-lg">
-                          <i className="fas fa-circle fa-stack-2x"></i>
-                          <i className="fab fa-twitter fa-stack-1x fa-inverse"></i>
-                        </span>
-                      </a>
-                    </li>
-                    <li className="list-inline-item">
-                      <a href="#">
-                        <span className="fa-stack fa-lg">
-                          <i className="fas fa-circle fa-stack-2x"></i>
-                          <i className="fab fa-facebook-f fa-stack-1x fa-inverse"></i>
-                        </span>
-                      </a>
-                    </li>
-                    <li className="list-inline-item">
-                      <a href="#">
-                        <span className="fa-stack fa-lg">
-                          <i className="fas fa-circle fa-stack-2x"></i>
-                          <i className="fab fa-github fa-stack-1x fa-inverse"></i>
-                        </span>
-                      </a>
-                    </li>
-                  </ul>
-                  <p className="copyright text-muted">Copyright &copy; Your Website 2019</p>
-                </div>
-              </div>
-            </div>
-          </footer>
+          <Footer/>
         </div>
       );
   }
